0452a53d39ff63bc331f99fbc31bfd9fecda5988
Tim <timkentdev@gmail.com>
2026-01-27 21:39:49 -0800

    chore: import sass styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	// Silence Sass deprecation warnings.
+  css: {
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     },
+  },
 });
