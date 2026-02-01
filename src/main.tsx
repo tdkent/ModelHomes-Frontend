bf534d8b535c4fff05fe285f72501a0c1f5fe4b6
Tim <timkentdev@gmail.com>
2026-02-01 14:54:49 -0800

    chore: remove bootstrap, add shadcn and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
-import "./styles/styles.scss";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
