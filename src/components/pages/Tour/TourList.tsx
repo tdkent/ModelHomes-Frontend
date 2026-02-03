a01994df9f25dd944f2ce387c22a88cd4954e64a
Tim <timkentdev@gmail.com>
2026-02-03 12:00:54 -0800

    add tour list component

---
+++ src/components/pages/Tour/TourList.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { BACKEND_URL } from "@/constants/constants";
+
+export default function TourList() {
+	const { isPending, error, data } = useQuery({
+		queryKey: ["homes"],
+		queryFn: async () => {
+			const response = await fetch(`${BACKEND_URL}/homes`);
+			return await response.json();
+		},
+		staleTime: 24 * 60 * 60 * 1000, // 1d
+	});
+
+	console.log(isPending, error, data);
+
+	return <>Tour List</>;
+}
