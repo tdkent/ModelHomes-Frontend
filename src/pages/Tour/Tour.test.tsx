8cb0e59db973950577c6d9eadae45607d7869887
Tim <timkentdev@gmail.com>
2026-02-03 12:32:01 -0800

    add query client provider to tests

--- src/pages/Tour/Tour.test.tsx
+++ src/pages/Tour/Tour.test.tsx
@@ -1,17 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
 import TourPage from "@/pages/Tour/Tour.page";
 import { mockedHomesData } from "@/test/mocks";
 
+const queryClient = new QueryClient();
+
 describe("Tour page", () => {
 	// const user = userEvent.setup();
 	it("fetches a list of homes", async () => {
-		render(<TourPage />);
+		render(
+			<QueryClientProvider client={queryClient}>
+				<TourPage />
+			</QueryClientProvider>,
+		);
 
 		// Test loading state
 		expect(screen.getByText(/loading.../i)).toBeInTheDocument();
 
-		const list = await screen.getByRole("list"); // nav list will interfere?
+		const list = await screen.findByRole("list"); // nav list will interfere?
 		expect(list).toBeInTheDocument();
 		expect(list.childElementCount).toBe(3);
 
