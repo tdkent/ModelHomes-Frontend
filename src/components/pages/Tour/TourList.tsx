dc911156a960b04bfd51a8950c569ec52d86987c
Tim <timkentdev@gmail.com>
2026-02-04 17:04:08 -0800

    create getHomes query function

--- src/components/pages/Tour/TourList.tsx
+++ src/components/pages/Tour/TourList.tsx
@@ -1,16 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import httpRequest from "@/api/httpRequest";
 import Loading from "@/components/Loading";
 import TourListItem from "@/components/pages/Tour/TourListItem";
-import { BACKEND_URL } from "@/constants/constants";
+// import { BACKEND_URL } from "@/constants/constants";
 import type { ModelHome } from "@/types/types";
 
 export default function TourList() {
 	const { isPending, error, data } = useQuery({
 		queryKey: ["homes"],
-		queryFn: async () => {
-			const response = await fetch(`${BACKEND_URL}/homes`);
-			return await response.json();
-		},
+		queryFn: () => httpRequest("/homes"),
 		staleTime: 24 * 60 * 60 * 1000, // 1d
 	});
 
