35212acc7fe04dd4f63a89b93df183239865b0f5
Tim <timkentdev@gmail.com>
2026-02-09 10:13:31 -0800

    add icon and set ratios while images load

--- src/components/shared/Image.tsx
+++ src/components/shared/Image.tsx
@@ -1,3 +1,5 @@
+import { Image as ImageIcon } from "lucide-react";
+import { useState } from "react";
 import { ASSETS_URL } from "@/constants/constants";
 import createSrcSets from "@/helpers/createSrcSet";
 
@@ -19,11 +21,21 @@ export default function Image({
 	isHero,
 	position,
 }: Props) {
+	const [isLoading, setIsLoading] = useState(true);
 	const srcSets = createSrcSets(homeId, imageId);
+
+	// Specify aspect ratio to avoid layout shift after load
+	const loadingAspectRatio = isLoading ? "aspect-5/3" : "aspect-auto";
+
 	return (
 		<div
-			className={`w-full overflow-hidden ${isHero ? "aspect-2/1" : "aspect-auto"}`}
+			className={`w-full overflow-hidden ${isHero ? "aspect-2/1" : loadingAspectRatio}`}
 		>
+			{isLoading && (
+				<div className="w-full h-full bg-black/5">
+					<ImageIcon className="stroke-white size-12 stroke-1" />
+				</div>
+			)}
 			<picture className="flex w-full h-full">
 				<source srcSet={srcSets.avif} sizes={sizes} type="image/avif" />
 				<source srcSet={srcSets.webp} sizes={sizes} type="image/webp" />
@@ -33,6 +45,7 @@ export default function Image({
 					sizes={sizes}
 					loading={lazy ? "lazy" : "eager"}
 					className={`object-cover w-full h-auto ${isHero && "blur-xs sepia"} ${position}`}
+					onLoad={() => setIsLoading(false)}
 				/>
 			</picture>
 		</div>
