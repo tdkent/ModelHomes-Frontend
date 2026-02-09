6889c82ec97e03003a9cf7e689046687fdf61ee7
Tim <timkentdev@gmail.com>
2026-02-09 15:37:31 -0800

    add home page header img

--- src/components/pages/Home/Header.tsx
+++ src/components/pages/Home/Header.tsx
@@ -1,10 +1,17 @@
 import Image from "@/components/shared/Image";
-import { SITE_NAME } from "@/constants/constants";
+import { ASSETS_URL, SITE_NAME } from "@/constants/constants";
+import createSrcSets from "@/helpers/createSrcSet";
 
 export default function Header() {
+	const baseUrl = `${ASSETS_URL}/ggie/ggie-ti-postcard`;
 	return (
 		<header>
-			{/* <Image /> */}
+			<Image
+				altText="Postcard of Golden Gate International Exposition, Treasure Island"
+				aspectRatio="aspect-2/1"
+				imgUrl={`${baseUrl}@1280.jpeg`}
+				srcSets={createSrcSets(baseUrl)}
+			/>
 			<h1>{SITE_NAME}</h1>
 		</header>
 	);
