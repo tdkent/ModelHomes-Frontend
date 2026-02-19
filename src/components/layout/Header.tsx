f06e739adc60b63541c33b13d72a106589b7cefe
Tim <timkentdev@gmail.com>
2026-02-18 20:38:00 -0800

    conditionally render bg image on home page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,22 @@
+import { useLocation } from "react-router";
 import Navigation from "@/components/layout/Navigation";
+import { SITE_NAME } from "@/constants/constants";
 
 export default function Header() {
+	const { pathname } = useLocation();
+	const isHome = pathname === "/";
 	return (
 		<>
-			<header className="flex items-center justify-between py-4 px-6">
-				<div className="font-heading uppercase text-2xl">g.g.i.e.m.h.</div>
-				<Navigation />
+			<header
+				className={`py-4 px-6 ${isHome && "bg-[url(/img/ggie-ti-postcard@1280.jpeg)] bg-cover w-full aspect-square"}`}
+			>
+				<div className="flex items-center justify-between">
+					<div className="font-heading uppercase text-2xl">g.g.i.e.m.h.</div>
+					<Navigation />
+				</div>
+				{isHome && (
+					<h1 className="text-background text-4xl font-heading">{SITE_NAME}</h1>
+				)}
 			</header>
 		</>
 	);
