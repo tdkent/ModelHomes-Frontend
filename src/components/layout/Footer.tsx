09b806a83207a0a3292a25039c040d269df15e0d
Tim <timkentdev@gmail.com>
2026-02-28 19:55:36 -0800

    add footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="border-t p-4 text-sm">
+			© 2026. All rights reserved.
+		</footer>
+	);
+}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router";
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 export default function RootLayout() {
 	return (
 		<>
 			<div>
 				<Header />
-				<main>
+				<main className="mx-4 my-10">
 					<Outlet />
 				</main>
-				<footer>© 2026. All rights reserved.</footer>
+				<Footer />
 			</div>
 		</>
 	);
