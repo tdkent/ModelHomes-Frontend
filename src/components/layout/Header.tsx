eabdfb12e1c3448ee3762103952f3366cebc0238
Tim <timkentdev@gmail.com>
2026-03-03 12:58:22 -0800

    add inner container to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,15 @@
-import { useLocation } from "react-router";
 import Navigation from "@/components/layout/Navigation";
 
 export default function Header() {
-	const { pathname } = useLocation();
-	const isHome = pathname === "/";
 	return (
 		<>
 			<header
-				className={`absolute top-0 right-0 flex items-center justify-between z-50 w-full min-h-16 px-4 bg-transparent ${isHome ? "text-background" : "text-foreground"}`}
+				className={`absolute top-0 right-0 flex items-center justify-center z-50 w-full min-h-16 bg-transparent`}
 			>
-				<div className="font-sans text-xl">g.g.i.e. model homes</div>
-				<Navigation />
+				<div className="w-fit flex items-center justify-between gap-16 bg-white rounded-2xl px-6">
+					<div className="font-sans text-lg">g.g.i.e. model homes</div>
+					<Navigation />
+				</div>
 			</header>
 		</>
 	);
