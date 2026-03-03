e5303b3ae1f58ad6fd67af7fc0df375b46f08aa4
Tim <timkentdev@gmail.com>
2026-03-03 10:46:19 -0800

    add splash component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,36 +1,17 @@
-// import { useLocation } from "react-router";
+import { useLocation } from "react-router";
 import Navigation from "@/components/layout/Navigation";
 
 export default function Header() {
-	// const { pathname } = useLocation();
-	// const isHome = pathname === "/";
+	const { pathname } = useLocation();
+	const isHome = pathname === "/";
 	return (
 		<>
-			<header className="flex items-center justify-between z-50 w-full min-h-16 px-4 bg-transparent">
+			<header
+				className={`absolute top-0 right-0 flex items-center justify-between z-50 w-full min-h-16 px-4 bg-transparent ${isHome ? "text-background" : "text-foreground"}`}
+			>
 				<div className="font-sans text-xl">g.g.i.e. model homes</div>
 				<Navigation />
 			</header>
-			{/* <header
-				className={`${isHome && "bg-[url(/img/ggie-ti-postcard@1280.jpeg)] bg-position-[35%_center] bg-no-repeat bg-cover w-full h-screen"}`}
-			>
-				<div className="w-full h-full backdrop-blur-[2px]">
-					<div
-						className={`px-4 py-3 flex items-center justify-between ${isHome ? "text-background" : "text-foreground border-b"}`}
-					>
-						<div className="font-sans text-xl">g.g.i.e. model homes</div>
-						<Navigation />
-					</div>
-					{isHome && (
-						<div className="flex mt-28 h-full px-4">
-							<h1 className="bg-linear-to-r from-yellow-100 to-blue-200 bg-clip-text text-transparent text-[44px] font-sans font-bold leading-12 opacity-90">
-								Model Homes <span className="text-3xl block my-2">of the</span>{" "}
-								Golden Gate International Exposition{" "}
-								<span className="text-3xl block mt-4">1939-40</span>
-							</h1>
-						</div>
-					)}
-				</div>
-			</header> */}
 		</>
 	);
 }
---
+++ src/components/pages/Homes/Splash.tsx
@@ -0,0 +1,15 @@
+export default function Splash() {
+	return (
+		<>
+			<div className="w-full h-lvh bg-[url(/img/ggie-ti-postcard@1280.jpeg)] bg-position-[35%_center] bg-no-repeat bg-cover">
+				<div className="w-full h-full backdrop-blur-[2px]">
+					<h1 className="relative bg-linear-to-r from-yellow-100 to-blue-200 bg-clip-text text-transparent text-[44px] font-sans font-bold leading-12 opacity-90 top-28 px-4">
+						Model Homes <span className="text-3xl block my-2">of the</span>{" "}
+						Golden Gate International Exposition{" "}
+						<span className="text-3xl block mt-4">1939-40</span>
+					</h1>
+				</div>
+			</div>
+		</>
+	);
+}
