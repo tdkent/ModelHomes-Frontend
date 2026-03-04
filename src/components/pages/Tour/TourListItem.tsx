cfb6fb3e42d29b0752d277a02b08f99105010ed5
Tim <timkentdev@gmail.com>
2026-03-03 22:04:45 -0800

    update tour list items

--- src/components/pages/Tour/TourListItem.tsx
+++ src/components/pages/Tour/TourListItem.tsx
@@ -10,15 +10,17 @@ interface Props {
 export default function TourListItem({ home }: Props) {
 	const id = home.id;
 	return (
-		<li>
+		<li className="even:bg-slate-50">
 			<Link to={`/homes/${id}`} aria-label={`Home ${id}`}>
-				<div className="flex items-center justify-between gap-4 px-4 py-2.5 text-slate-700">
-					<div className="flex gap-5">
+				<div className="flex items-center justify-between gap-4 px-4 py-2.5 text-slate-700 lg:py-3">
+					<div className="flex gap-5 lg:gap-8 items-center">
 						<Thumbnail id={id} imgId={home.images.thumbnail} />
 						<div className="flex flex-col font-sans">
-							<span className="text-base">Model Home #{id}</span>
-							<span className="text-sm">City: {home.city}</span>
-							<span className="text-sm">County: {home.county}</span>
+							<span className="text-base lg:text-lg">Model Home #{id}</span>
+							<span className="text-sm lg:text-base">City: {home.city}</span>
+							<span className="text-sm lg:text-base">
+								County: {home.county}
+							</span>
 						</div>
 					</div>
 					<ChevronRightIcon className="stroke-1 size-6" />
