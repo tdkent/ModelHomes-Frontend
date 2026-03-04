d9c13dc5bcc0be34d0f825f146ed5be19bc1eb13
Tim <timkentdev@gmail.com>
2026-03-03 16:55:28 -0800

    Style: tablet first pass CSS (#56)
    * update splash for sm and md breakpoints
    
    * style: mobile nav for sm and md breakpoints

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,10 +4,12 @@ export default function Header() {
 	return (
 		<>
 			<header
-				className={`absolute top-0 right-0 flex items-center justify-center z-50 w-full min-h-16 bg-transparent`}
+				className={`absolute top-0 right-0 flex items-center justify-center z-50 w-full min-h-16 bg-transparent px-4 py-6 lg:text-background`}
 			>
-				<div className="w-fit flex items-center justify-between gap-16 bg-white rounded-2xl px-6">
-					<div className="font-sans text-lg">g.g.i.e. model homes</div>
+				<div className="flex items-center justify-between bg-white rounded-2xl px-4 w-full sm:py-2 sm:px-8 lg:bg-transparent lg:p-8">
+					<div className="font-sans text-lg sm:text-2xl md:text-2xl">
+						g.g.i.e. model homes
+					</div>
 					<Navigation />
 				</div>
 			</header>
