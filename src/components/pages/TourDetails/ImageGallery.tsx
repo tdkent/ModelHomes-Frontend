cbb353b7526ec1692bd582bc6b798c8f146593d5
Tim <timkentdev@gmail.com>
2026-03-04 09:51:43 -0800

    Feat: clickable fullscreen images (#58)
    * add shadcn dialog to clickable gallery imgs
    
    * configure dialog for fs display with preserved image ar. overflow added since images will not fit perfectly

--- src/components/pages/TourDetails/ImageGallery.tsx
+++ src/components/pages/TourDetails/ImageGallery.tsx
@@ -1,5 +1,17 @@
+import { X } from "lucide-react";
 import Image from "@/components/shared/Image";
 import TextHeading from "@/components/shared/TextHeader";
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
 import { ASSETS_URL } from "@/constants/constants";
 import createSrcSets from "@/helpers/createSrcSet";
 
@@ -17,13 +29,46 @@ export default function ImageGallery({ id, gallery }: Props) {
 				{gallery.map((img) => {
 					const baseUrl = `${ASSETS_URL}/home-${id}/home-${img}`;
 					return (
-						<Image
-							altText={`Model home #${id}`}
-							key={img}
-							srcSets={createSrcSets(baseUrl)}
-							imgUrl={`${baseUrl}@1280s.jpeg`}
-							lazy
-						/>
+						<Dialog key={img}>
+							<DialogTrigger className="cursor-pointer scale:100 hover:scale-105 transition duration-300">
+								<Image
+									altText={`Model home #${id}`}
+									key={img}
+									srcSets={createSrcSets(baseUrl)}
+									imgUrl={`${baseUrl}@1280.jpeg`}
+									lazy
+								/>
+							</DialogTrigger>
+							<DialogContent className="min-w-full h-full overflow-auto flex items-center justify-center bg-black/75 rounded-none border-none p-0 [&>button]:hidden">
+								<DialogHeader className="sr-only">
+									<DialogTitle>`Model Home #${id}`</DialogTitle>
+									<DialogDescription>
+										Full-screen image of Model Home #${id}
+									</DialogDescription>
+								</DialogHeader>
+
+								<img
+									src={`${baseUrl}@1280.jpeg`}
+									alt={`Model home #${id}`}
+									className="object-cover"
+								/>
+
+								<DialogFooter className="fixed right-2 top-2 scale-90 sm:justify-start md:right-4 md:top-4 lg:right-8 lg:top-8">
+									<DialogClose asChild>
+										<Button
+											aria-label="Close"
+											className="cursor-pointer font-sans opacity-90 text-xs md:text-sm lg:text-lg"
+											type="button"
+											variant="secondary"
+											size="sm"
+										>
+											<X className="size-4 md:size-5 lg:size-6" />
+											Close
+										</Button>
+									</DialogClose>
+								</DialogFooter>
+							</DialogContent>
+						</Dialog>
 					);
 				})}
 			</div>
