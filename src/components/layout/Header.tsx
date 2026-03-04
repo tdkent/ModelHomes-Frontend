2d82854e00a76fd2973564100e14c0ebe9b62b6c
Tim <timkentdev@gmail.com>
2026-03-03 21:46:33 -0800

    update layout and splash

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
 			<header
 				className={`absolute top-0 right-0 flex items-center justify-center z-50 w-full min-h-16 bg-transparent px-4 py-6 ${isHome ? "lg:text-background" : "lg:text-foreground"}`}
 			>
-				<div className="flex items-center justify-between bg-white rounded-2xl px-4 w-full sm:py-2 sm:px-8 lg:bg-transparent lg:p-8">
-					<div className="font-sans text-lg sm:text-2xl md:text-2xl">
+				<div className="flex items-center justify-between bg-white rounded-2xl px-4 w-full sm:py-2 sm:px-8 lg:bg-transparent lg:p-8 xl:px-16">
+					<div className="font-sans text-lg sm:text-2xl md:text-2xl xl:text-3xl">
 						g.g.i.e. model homes
 					</div>
 					<Navigation />
--- src/components/layout/nav/DesktopNav.tsx
+++ src/components/layout/nav/DesktopNav.tsx
@@ -6,14 +6,14 @@ export default function DesktopNav() {
 	return (
 		<>
 			<nav className="max-lg:hidden font-sans">
-				<ul className="flex items-center gap-8">
+				<ul className="flex items-center gap-8 lg:gap-12 xl:gap-20">
 					{navLinks.map((link) => {
 						return (
 							<li key={link.label}>
 								<NavLink
 									to={link.href}
 									data-testid={`${link.testId}-link`}
-									className="text-base"
+									className="text-base xl:text-lg"
 								>
 									{link.label}
 								</NavLink>
