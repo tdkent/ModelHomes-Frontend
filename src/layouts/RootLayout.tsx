ab8854cf4758544901db03782634a68cec8c81c5
Tim <timkentdev@gmail.com>
2026-03-08 20:08:25 -0700

    preconnect, preload google fonts resources

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { Outlet } from "react-router";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
@@ -5,6 +6,14 @@ import Header from "@/components/layout/Header";
 export default function RootLayout() {
 	return (
 		<>
+			<Helmet>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preload"
+					as="style"
+					href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+				/>
+			</Helmet>
 			<div id="container" className="h-screen flex flex-col">
 				<Header />
 				<main className="flex-1 lg:flex lg:flex-col lg:items-center">
